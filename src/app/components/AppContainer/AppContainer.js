ff3e8a163a726cc4c0852e7ed7cea46ad31d6b08
Ben Brewer <benbrewerbowman@gmail.com>
2017-10-26 19:22:37 -0600

    Scalable project structure implemented

---
+++ src/app/components/AppContainer/AppContainer.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import { default as App } from './App/App';
+
+export default App;
---
+++ src/app/components/index.js
@@ -0,0 +1,2 @@
+// exports here
+// i.e. export { default as Component } from './SomeComponent/Component';
