82c283158ee578fcc095ae412ac1b1fd4905b306
Ben Brewer <benbrewerbowman@gmail.com>
2017-11-16 10:13:19 -0700

    SVG react loader added

---
+++ src/app/utils/images/svg-icon.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SvgIcon = (props) => {
+  return(
+    <div className={props.className ? props.className : "SvgIcon"}>
+      <svg
+        role="img"
+        width={props.width ? props.width : 24}
+        height={props.height ? props.height : 24}
+        fill={props.color ? props.color : 'black'}
+        viewBox={props.viewBox ? props.viewBox : "0 0 24 24" }
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d={props.path}
+        />
+      </svg>
+    </div>
+  );
+}
+
+SvgIcon.propTypes = {
+  className: PropTypes.string,
+  width: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+  height: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+  ]),
+  color: PropTypes.string,
+  viewBox: PropTypes.string,
+  path: PropTypes.string.isRequired
+}
+
+export default SvgIcon;
